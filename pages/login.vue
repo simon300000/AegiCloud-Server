3ce0c5ec4b9d969dcb77cae5b67fd52e1deb987d
Il Harper <afanyiyu@hotmail.com>
2019-08-13 14:52:41 +0800

    Login Layout Changes

--- pages/login.vue
+++ pages/login.vue
@@ -6,37 +6,16 @@
           <v-col cols="12" sm="8" md="4">
             <v-card class="elevation-12">
               <v-toolbar color="primary" dark flat>
-                <v-toolbar-title>Login form</v-toolbar-title>
+                <svgicon icon-class="logo" height="50" width="110" />
+                <v-toolbar-title>AegiCloud Dashboard</v-toolbar-title>
                 <v-spacer></v-spacer>
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn :href="source" icon large target="_blank" v-on="on">
-                      <v-icon>mdi-code-tags</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                  <template v-slot:activator="{ on }">
-                    <v-btn
-                      icon
-                      large
-                      href="https://codepen.io/johnjleider/pen/pMvGQO"
-                      target="_blank"
-                      v-on="on"
-                    >
-                      <v-icon>mdi-codepen</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Codepen</span>
-                </v-tooltip>
               </v-toolbar>
               <v-card-text>
                 <v-form>
                   <v-text-field
-                    label="Login"
+                    label="Username"
                     name="login"
-                    prepend-icon="person"
+                    prepend-icon="mdi-login"
                     type="text"
                   ></v-text-field>
 
@@ -44,7 +23,7 @@
                     id="password"
                     label="Password"
                     name="password"
-                    prepend-icon="lock"
+                    prepend-icon="mdi-lock"
                     type="password"
                   ></v-text-field>
                 </v-form>
@@ -63,11 +42,6 @@
 
 <script>
 export default {
-  data: () => ({
-    drawer: null
-  }),
-  mounted() {
-    this.$vuetify.theme.dark = true
-  }
+  data: () => ({})
 }
 </script>
