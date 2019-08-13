a296d883f660d62dfd77798fdc13ff051d5a5564
Il Harper <afanyiyu@hotmail.com>
2019-08-13 14:31:29 +0800

    Pages Changes

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app clipped-left>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col class="shrink">
+            <h1>npm累觉不爱</h1>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; AegiCloud 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+}
+</script>
