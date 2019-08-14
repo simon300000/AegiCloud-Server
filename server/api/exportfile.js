c7499cff1d0193ce355f96da50be9bffb13ea991
Il Harper <afanyiyu@hotmail.com>
2019-08-14 20:29:40 +0800

    /api/exportfile Fix

--- server/api/exportfile.js
+++ server/api/exportfile.js
@@ -1,6 +1,7 @@
-const fs = require('fs')
+// const fs = require('fs')
+const send = require('koa-send')
 const Router = require('koa-router')
-const coreexportfile = require('../core/export')
+// const coreexportfile = require('../core/export')
 const exportfile = new Router()
 
 // /exportfile
@@ -13,17 +14,25 @@ exportfile.post('/', async (ctx, next) => {
   if (ctx.request.body.filename) {
     try {
       ctx.response.status = 200
-      global.data.conf.filename = ctx.request.body.filename
-      const file = coreexportfile(ctx.request.body.filename)
-      const size = (await fs.promises.stat(file.path)).size
-      const f = fs.createReadStream(file.path)
-      ctx.response.status = 200
-      ctx.response.set({
-        'Content-Type': 'application/force-download',
-        'Content-Disposition': 'attachment; filename=' + file.name,
-        'Content-Length': size
-      })
-      f.pipe(ctx.response)
+      // global.data.conf.filename = ctx.request.body.filename
+      // const file = coreexportfile(ctx.request.body.filename)
+      // const size = (await fs.promises.stat(file.path)).size
+      // const f = fs.createReadStream(file.path)
+      // ctx.response.status = 200
+      // ctx.response.set({
+      //   'Content-Type': 'application/force-download',
+      //   'Content-Disposition': 'attachment; filename=' + file.name,
+      //   'Content-Length': size
+      // })
+      // f.pipe(ctx.response)
+      // console.log(file)
+      ctx.attachment(
+        '/aegicloud/projects/' + ctx.request.body.filename.toString()
+      )
+      await send(
+        ctx,
+        '/aegicloud/projects/' + ctx.request.body.filename.toString()
+      )
     } catch (error) {
       ctx.response.status = 500
       ctx.response.message = error
